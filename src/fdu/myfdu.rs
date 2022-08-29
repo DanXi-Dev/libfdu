61aee250a5053da9a0168bf21803ff32c9e83b5a
Frankstein73 <1053905229@qq.com>
2022-08-29 15:54:57 +0800

    fix: myfdu support

--- src/fdu/myfdu.rs
+++ src/fdu/myfdu.rs
@@ -7,12 +7,12 @@ use crate::fdu::fdu::{Account, Fdu};
 
 const MYFDU_URL: &str = "https://my.fudan.edu.cn/";
 const COURSE_GRADE_URL: &str = "https://my.fudan.edu.cn/list/bks_xx_cj";
-
+#[derive(Debug)]
 pub struct GradeData {
     id: String,
     name: String,
-    academic_year: (i32, i32),
-    semester: i32,
+    academic_year: String,
+    semester: String,
     credits: f32,
     grade: String,
 }
@@ -20,11 +20,27 @@ pub struct GradeData {
 impl MyFduClient for Fdu {}
 
 pub trait MyFduClient: Account {
-    fn get_myfdu_course_grade(&self) -> reqwest::Result<String>{
+    fn get_myfdu_course_grade(&self) -> reqwest::Result<Vec<GradeData>> {
         let client = self.get_client();
-        let html  = client.get(COURSE_GRADE_URL).send()?;
-        println!("{:?}", html);
-        Ok("das".to_string())
+        let html = client.get(COURSE_GRADE_URL).send()?.text()?;
+        let document = Html::parse_document(html.as_str());
+        let selector = Selector::parse("#dataTable_BksXxCj>tbody>tr").unwrap();
+        let mut grade_data: Vec<GradeData> = Vec::new();
+        for element in document.select(&selector) {
+            let sub_selector = Selector::parse("td").unwrap();
+            let mut sub_element = element.select(&sub_selector);
+            let course_info: GradeData = GradeData {
+                id: sub_element.next().unwrap().inner_html(),
+                academic_year: sub_element.next().unwrap().inner_html(),
+                semester: sub_element.next().unwrap().inner_html(),
+                name: sub_element.next().unwrap().inner_html(),
+                credits: sub_element.next().unwrap().inner_html().parse().unwrap(),
+                grade: sub_element.next().unwrap().inner_html(),
+            };
+            // println!("{:?}", course_info);
+            grade_data.push(course_info);
+        }
+        Ok(grade_data)
     }
 }
 
@@ -42,6 +58,7 @@ mod tests {
 
         let mut fd = Fdu::new();
         fd.login(uid.as_str(), pwd.as_str()).expect("login error");
-        fd.get_myfdu_course_grade();
+        fd.get_myfdu_course_grade().expect("my fdu error");
+        fd.logout().expect("logout error");
     }
 }
